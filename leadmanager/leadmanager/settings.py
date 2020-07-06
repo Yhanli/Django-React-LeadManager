879a6396da1a7124a5b5d2f35cf6c6ee8edabc37
Yhanli <45836848+Yhanli@users.noreply.github.com>
2020-07-06 16:33:25 +1200

    change static

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/static/frontend/'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static/frontend/')
+STATIC_URL = '/frontend/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/')
