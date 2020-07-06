af6ae7d2cf6e81cf01200cfd1d77e9872251fcdf
Yhanli <45836848+Yhanli@users.noreply.github.com>
2020-07-06 16:37:56 +1200

    change static

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -128,4 +128,4 @@ USE_TZ = True
 
 STATIC_URL = '/frontend/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/frontend/'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/frontend/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
