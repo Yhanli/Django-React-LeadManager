f4b9f15cd0e9ed64d96ae0d0f53d2b764bef7d8f
Yhanli <45836848+Yhanli@users.noreply.github.com>
2020-07-06 15:45:02 +1200

    changed static url

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -127,5 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-STATIC_ROOT =os.path.join(BASE_DIR, 'static_root/')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/frontend/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/frontend/')
