680cc4901362e47de52c749250e9a7f64e7f0040
yhanli <yhanl@hotmail.com>
2021-05-01 15:57:49 +1200

    add pull quick command

---
+++ pull.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo git stash
+sudo git reset --hard origin/master
+sudo git pull
+sudo npm install
+sudo npm run build
+pipenv sync
+pipenv run python leadmanager/manage.py makemigrations
+pipenv run python leadmanager/manage.py migrate
+pipenv run python leadmanager/manage.py collectstatic --noinput
+sudo supervisorctl stop gunicornLead
+sudo supervisorctl start gunicornLead
