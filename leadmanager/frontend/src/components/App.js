1d97e0f0ebfb09294f1c5260fcbbf8a46860058c
yhanli <yhanl@hotmail.com>
2021-05-03 22:41:33 +1200

    change alert style

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import { Provider as AlertProvider} from 'react-alert';
-import AlertTemplate from "react-alert-template-basic";
+import AlertTemplate from "./react-alert-template";
 
 import Header from './layout/Header';
 import Footer from './layout/Footer';
